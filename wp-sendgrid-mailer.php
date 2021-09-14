4fe96abd1f32f2836f23fc6f6191e6143d6fdf16
admin <admin@innoshop.co>
2021-09-14 21:31:35 +0000

    initial files

---
+++ wp-sendgrid-mailer.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+Plugin Name: WP SendGrid Mailer
+Plugin URI: https://github.com/devwael
+Description: Just another contact form plugin. Simple but flexible.
+Author: Takayuki Miyoshi
+Author URI: https://github.com/devwael
+Version: 1.0
+*/
+
